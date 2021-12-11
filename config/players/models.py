2d2013dc83e2e3b671918457f81be75cd416b540
Okakakai <kai48586@gmail.com>
2021-12-11 16:51:55 +0900

    [add] add models

--- config/players/models.py
+++ config/players/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+class player(models.Model):
+    # player Model
+
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable=False)
+    playername = models.CharField(max_length=200)
+    age = models.IntegerField(null = False)
+    nationality = models.CharField(max_length=200)
+    team = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now=True)

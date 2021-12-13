833694cd6a1b18456bfe397efb98903e84d4f084
Okakakai <kai48586@gmail.com>
2021-12-13 13:30:09 +0900

    fix models.py

--- config/players/models.py
+++ config/players/models.py
@@ -7,7 +7,7 @@ class Player(models.Model):
 
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable=False)
     playername = models.CharField(max_length=200)
-    age = models.IntegerField(null = False)
+    age = models.PositiveIntegerField(null = False)
     nationality = models.CharField(max_length=200)
     team = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add = True)

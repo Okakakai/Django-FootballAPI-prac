f746727dbbadff7e2c5b505e6f56848b9e63f3ad
Okakakai <kai48586@gmail.com>
2021-12-21 14:39:01 +0900

    rename filed from name to headcoachname

--- config/players/models/headCoach.py
+++ config/players/models/headCoach.py
@@ -36,7 +36,7 @@ class HeadCoach(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateField(auto_now_add=True)
 
-    name = models.CharField(max_length=50)
+    headcoachname = models.CharField(max_length=50)
     managementability = models.PositiveIntegerField(null=False)
     adaptability = models.PositiveIntegerField(null=False)
     formation = models.CharField(max_length=20, null=False)

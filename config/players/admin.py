c4ad483ee20e7128e8c61e5eb168e69d5405e8af
Okakakai <kai48586@gmail.com>
2021-12-21 14:29:26 +0900

    display headcoaches on admin page

--- config/players/admin.py
+++ config/players/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 
 from .models import Player
+from .models import HeadCoach
+
 
 class PlayerModelAdmin(admin.ModelAdmin):
-    list_display = ('playername','nationality','id','created_at')
+    list_display = ('playername', 'nationality', 'id', 'created_at')
     ordering = ('-created_at',)
-    readonly_fields = ('id','created_at')
+    readonly_fields = ('id', 'created_at')
 
 
 admin.site.register(Player, PlayerModelAdmin)
+
+
+class HeadCoachModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'managementability', 'id', 'created_at')
+    ordering = ('managementability',)
+    readonly_fields = ('id', 'created_at')
+
+
+admin.site.register(HeadCoach, HeadCoachModelAdmin)

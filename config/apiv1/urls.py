89724ebe79313f2b589c7131a28da5573aed69d4
Okakakai <kai48586@gmail.com>
2021-12-21 10:57:34 +0900

    add url PlayerPositionListView

--- config/apiv1/urls.py
+++ config/apiv1/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 router = routers.DefaultRouter()
 # /player/でviewにアクセスできるようにする
-router.register(r'players',views.PlayerViewSet)
+router.register(r'players', views.PlayerViewSet)
+# router.register(r'position', views.PlayerPositionListView)
+# router.register(r'age', views.KariViewSet)
 
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('position/', views.PlayerPositionListView.as_view())
 ]

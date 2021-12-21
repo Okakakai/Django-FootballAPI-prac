a0961bc3dd6240764b05045dbb77d17df3429fd9
Okakakai <kai48586@gmail.com>
2021-12-21 10:58:11 +0900

    PositionSerializer

--- config/apiv1/serializers.py
+++ config/apiv1/serializers.py
@@ -6,6 +6,14 @@ from players.models import Player
 class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
-        fields = ('id', 'date', 'playername', 'playerimage',
+        fields = ('id', 'date', 'playername', 'totalvalue', 'playerimage',
                   'nationality', 'team', 'updated_at')
         read_only_fields = ('id',)
+
+
+class PositionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Player
+        fields = ('id', 'date', 'nationality', 'playerimage', 'totalvalue', 'playername', 'mostsuitableposition_type',
+                  'suitableposition_type')

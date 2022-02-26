0994014dfa0195c380508aa2d69b72ac4c96b3c7
Okakakai <inoue.kai.work@gmail.com>
2022-02-26 22:32:39 +0900

    めんどくさい一括commit

---
+++ config/frontend/src/headers.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./css/header.css"
+
+export default function Headers(){
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink className="menu" to = "/">Top</NavLink>
+                </li>
+                <li>
+                    <NavLink className="menu" to = "/player">登録選手一覧</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+}
--- config/frontend/src/playerapi.js
+++ config/frontend/src/playerapi.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import "./playerapi.css"
+import "./css/playerapi.css"
 
 const PlayerAPI = () => {
     const [players, setplayers] = useState([]);

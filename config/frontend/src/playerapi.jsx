6b27015a9b457e5e71d31484063e80a6ee66bbac
Okakakai <inoue.kai.work@gmail.com>
2022-03-05 15:17:18 +0900

    にplayerの排出種類fpのみの一覧表示　プレイヤーの画像を詳細リンクに変更

--- config/frontend/src/playerapi.jsx
+++ config/frontend/src/playerapi.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import "./css/reset.css"
 import "./css/playerapi.css";
@@ -7,7 +8,7 @@ const PlayerAPI = () => {
     const [players, setplayers] = useState([]);
 
     useEffect(() => {
-        axios.get("http://127.0.0.1:8000/api/players/")
+        axios.get("http://127.0.0.1:8000/api/player-type/")
             .then(res => {
                 console.log("status:", res.status)
                 console.log("axios-data:", res.data)
@@ -16,20 +17,28 @@ const PlayerAPI = () => {
             .catch(err => {
                 console.log("axiosGetErr", err)
             })
+            
     }, [])
 
+    
+
     return ( 
-    <div className = "player-card-list" >
-        <ul> {
-            players.map(player =>
-                <li className = "playercard"key = { player.id } >
-                    <p className = "totalvalue" > { player.totalvalue } </p> 
-                    <img className = "playerimg"src = { player.playerimage }/>
-                    <p> { player.playername } </p> 
-                </li>
-            )
-        } </ul>  
-    </div >
+        <div className = "player-card-list" >
+            <div> {
+                <ul>{
+                    players.filter( player =>{
+                        return player.type == "FP"
+                    })
+                    .map(player =>
+                        <li key={player.id}>
+                            <Link to= {"/player-detail/" + player.id}>
+                                <img className = "playerimg"src = { player.playerimage }/>  
+                            </Link>
+                        </li>
+                    )
+                } </ul>
+            } </div>  
+        </div >
     )
 }
 
